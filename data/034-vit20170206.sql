4a17555573ce6009f132c54ee19817570f9fef78
vitvegl <vitvelg@gmail.com>
2017-04-25 22:47:30 +0300

    Workout 34

---
+++ data/034-vit20170206.sql
@@ -0,0 +1,49 @@
+start transaction;
+
+call set_athlet_id ('vit');
+
+insert into `workout` (`workout_date`, `workout_duration`, `athlet_id`)
+  values
+('2017-02-06', '0:46', (select @athlet_id));
+
+call set_workout_id ('vit', '2017-02-06');
+
+/* split */
+insert into `workout_type` (`wtype`) value ('split');
+
+insert into `exercise` (`description`, `weight_kg`, `reps`, `rest_time_sec`, `workout_id`)
+  values
+('Станова тяга', 33, 13, 120, (select @workout_id)),
+('Станова тяга', 33, 13, 120, (select @workout_id)),
+('Станова тяга', 33, 13, 120, (select @workout_id)),
+('Махи у сторони', 7, 10, 120, (select @workout_id)),
+('Махи у сторони', 7, 10, 120, (select @workout_id)),
+('Махи у сторони', 7, 10, 120, (select @workout_id)),
+('Махи у сторони', 7, 9, 120, (select @workout_id)),
+('Тяга 1 рукою у нахилі', 23, 14, 120, (select @workout_id)),
+('Тяга 1 рукою у нахилі', 23, 14, 120, (select @workout_id)),
+('Тяга 1 рукою у нахилі', 23, 14, 120, (select @workout_id)),
+('Жим сидячи', 13, 13, 120, (select @workout_id)),
+('Жим сидячи', 13, 11, 120, (select @workout_id)),
+('Жим сидячи', 13, 10, 120, (select @workout_id)),
+('Махи вперед', 5.5, 10, 90, (select @workout_id)),
+('Махи вперед', 5.5, 10, 90, (select @workout_id)),
+('Махи вперед', 5.5, 9, 90, (select @workout_id)),
+('Махи вперед', 5.5, 9, 90, (select @workout_id));
+
+set @workout_id = null;
+
+create view exercise_id as select e.id from exercise e
+  join workout w on e.workout_id = w.id
+  join athlet a on w.athlet_id = a.id
+where a.nickname = 'vit' and w.workout_date = '2017-02-06';
+
+/* без екіпірування */
+insert into `style` (`id`) select id from exercise_id;
+
+/* гантелі */
+insert into `equipment` (`id`) select id from exercise_id;
+
+drop view exercise_id;
+
+commit;
