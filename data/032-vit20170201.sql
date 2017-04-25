16882de095acdb93bc35325cd680b0029d4d43d2
vitvegl <vitvelg@gmail.com>
2017-04-25 22:47:04 +0300

    Workout 32

---
+++ data/032-vit20170201.sql
@@ -0,0 +1,47 @@
+start transaction;
+
+call set_athlet_id ('vit');
+
+insert into `workout` (`workout_date`, `workout_duration`, `athlet_id`)
+  values
+('2017-02-01', '0:45', (select @athlet_id));
+
+call set_workout_id ('vit', '2017-02-01');
+
+/* split */
+insert into `workout_type` (`wtype`) value ('split');
+
+insert into `exercise` (`description`, `weight_kg`, `reps`, `rest_time_sec`, `workout_id`)
+  values
+('Жим лежачи', 21, 16, 120, (select @workout_id)),
+('Жим лежачи', 21, 13, 120, (select @workout_id)),
+('Жим лежачи', 21, 5, 120, (select @workout_id)),
+('Французький жим лежачи', 11, 10, 90, (select @workout_id)),
+('Французький жим лежачи', 11, 10, 90, (select @workout_id)),
+('Французький жим лежачи', 11, 10, 90, (select @workout_id)),
+('Французький жим лежачи', 11, 10, 90, (select @workout_id)),
+('Пулл-овер стоячи', 17, 12, 120, (select @workout_id)),
+('Пулл-овер стоячи', 17, 10, 120, (select @workout_id)),
+('Пулл-овер стоячи', 17, 11, 120, (select @workout_id)),
+('Розведення рук лежачи', 15, 9, 90, (select @workout_id)),
+('Розведення рук лежачи', 15, 6, 90, (select @workout_id)),
+('Шраги', 21, 20, 90, (select @workout_id)),
+('Шраги', 21, 20, 90, (select @workout_id)),
+('Шраги', 21, 20, 90, (select @workout_id));
+
+set @workout_id = null;
+
+create view exercise_id as select e.id from exercise e
+  join workout w on e.workout_id = w.id
+  join athlet a on w.athlet_id = a.id
+where a.nickname = 'vit' and w.workout_date = '2017-02-01';
+
+/* без екіпірування */
+insert into `style` (`id`) select id from exercise_id;
+
+/* гантелі */
+insert into `equipment` (`id`) select id from exercise_id;
+
+drop view exercise_id;
+
+commit;
