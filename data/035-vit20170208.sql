194af11133a62f8594cc18e8c99768bf5de54c32
vitvegl <vitvelg@gmail.com>
2017-04-25 22:47:46 +0300

    Workout 35

---
+++ data/035-vit20170208.sql
@@ -0,0 +1,53 @@
+start transaction;
+
+call set_athlet_id ('vit');
+
+insert into `workout` (`workout_date`, `workout_duration`, `athlet_id`)
+  values
+('2017-02-08', '0:51', (select @athlet_id));
+
+call set_workout_id ('vit', '2017-02-08');
+
+/* split */
+insert into `workout_type` (`wtype`) value ('split');
+
+insert into `exercise` (`description`, `weight_kg`, `reps`, `rest_time_sec`, `workout_id`)
+  values
+('Жим лежачи', 21, 13, 120, (select @workout_id)),
+('Жим лежачи', 21, 13, 120, (select @workout_id)),
+('Жим лежачи', 21, 12, 120, (select @workout_id)),
+('Жим лежачи', 21, 8, 120, (select @workout_id)),
+('Пулл-овер стоячи', 17, 14, 120, (select @workout_id)),
+('Пулл-овер стоячи', 17, 13, 120, (select @workout_id)),
+('Пулл-овер стоячи', 17, 11, 120, (select @workout_id)),
+('Пулл-овер стоячи', 17, 13, 120, (select @workout_id)),
+('Пулл-овер стоячи', 17, 12, 120, (select @workout_id)),
+('Розведення рук лежачи', 13, 12, 90, (select @workout_id)),
+('Розведення рук лежачи', 13, 12, 90, (select @workout_id)),
+('Розведення рук лежачи', 13, 12, 90, (select @workout_id)),
+('Розведення рук лежачи', 13, 10, 90, (select @workout_id)),
+('Шраги', 21, 20, 120, (select @workout_id)),
+('Шраги', 21, 20, 120, (select @workout_id)),
+('Шраги', 21, 22, 120, (select @workout_id)),
+('Віджимання від підлоги', 0, 10, 90, (select @workout_id)),
+('Віджимання від підлоги', 0, 9, 90, (select @workout_id));
+
+set @workout_id = null;
+
+create view exercise_id as select e.id from exercise e
+  join workout w on e.workout_id = w.id
+  join athlet a on w.athlet_id = a.id
+where a.nickname = 'vit' and w.workout_date = '2017-02-08';
+
+/* без екіпірування */
+insert into `style` (`id`) select id from exercise_id;
+
+/* гантелі */
+insert into `equipment` (`id`) select id from exercise_id;
+
+/* вправи із вагою тіла */
+call update_exercise_if_no_weight ('vit', '2017-02-08');
+
+drop view exercise_id;
+
+commit;
